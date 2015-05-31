ff6a78f2cd5daeaa159954d293cb101655b742a3
Yoshiya Hinosawa <stibium121@gmail.com>
2015-06-01 00:33:19 +0900

    Fix BoneTree and add tests

--- spec/domain/BoneAnimationFactory.js
+++ spec/domain/BoneAnimationFactory.js
@@ -35,7 +35,6 @@ describe('BoneAnimation', function () {
 
         it('creates a BoneAnimation from the object', function () {
 
-            console.log(boneAnimationList[0]);
             var boneAnimation = this.factory.createFromObject(boneAnimationList[0]);
 
             expect(boneAnimation).to.be.instanceof(BoneAnimation);
--- spec/domain/BoneTree.js
+++ spec/domain/BoneTree.js
@@ -33,4 +33,38 @@ describe('BoneTree', function () {
         });
     });
 
+    describe('getParent', function () {
+
+        it('gets the parent', function () {
+
+            var bone = this.boneTree.boneOfId('0-0-0');
+
+            var parent = this.boneTree.getParent(bone);
+
+            expect(parent).to.be.instanceof(Bone);
+            expect(parent.id).to.equal('0-0');
+
+        });
+
+    });
+
+    describe('getParents', function () {
+
+        it('gets the list of the parents', function () {
+
+            var bone = this.boneTree.boneOfId('0-0-0');
+
+            var parents = this.boneTree.getParents(bone);
+
+            expect(parents).to.be.an('array');
+
+            expect(parents[0]).to.be.instanceof(Bone);
+            expect(parents[0].id).to.equal('0-0');
+            expect(parents[1]).to.be.instanceof(Bone);
+            expect(parents[1].id).to.equal('0');
+
+        });
+
+    });
+
 });
