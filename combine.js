a57199468e65e005998b6a823754606691faf44e
Yoshiya Hinosawa <stibium121@gmail.com>
2015-06-07 14:25:01 +0900

    Add error handlings

--- combine.js
+++ combine.js
@@ -18,11 +18,40 @@ console.log('[bone]   ' + bone);
 console.log('[anim]   ' + anim);
 console.log('[output] ' + output);
 
+if (!source) {
+    throw new Error('source not specified: ' + source);
+}
+
+if (!bone) {
+    throw new Error('bone not specified: ' + bone);
+}
+
+if (!anim) {
+    throw new Error('anim not specified: ' + anim);
+}
+
+if (!output) {
+    throw new Error('output not specified: ' + output);
+}
+
+
 
 var sourceSvg = fs.readFileSync(source).toString();
 
-bone = JSON.parse(fs.readFileSync(bone));
-anim = JSON.parse(fs.readFileSync(anim));
+try {
+    bone = JSON.parse(fs.readFileSync(bone));
+} catch (e) {
+    throw new Error('bone file is broken');
+}
+
+try {
+    anim = JSON.parse(fs.readFileSync(anim));
+} catch (e) {
+    throw new Error('anim file is broken');
+}
+
+console.log('All arguments look well');
+console.log('Generating output file');
 
 var outCss = ba2css(bone, anim);
 
